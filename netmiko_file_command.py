aa6225aa3cff4610e1742594be46eb7b5c32a87c
dele <oyedeloy@gmail.com>
2020-06-26 01:54:39 +0000

    my third git

--- netmiko_file_command.py
+++ netmiko_file_command.py
@@ -5,21 +5,21 @@ from netmiko import ConnectHandler
 iosv_l2_s4 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.84',
-    'username': 'david',
+    'username': 'dele',
     'password': 'cisco',
 }
 
 iosv_l2_s5 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.85',
-    'username': 'david',
+    'username': 'dele',
     'password': 'cisco',
 }
 
 iosv_l2_s6 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.86',
-    'username': 'david',
+    'username': 'dele',
     'password': 'cisco',
 }
 
